4225ec7e6bcb56f72ae330f18803355a87ef3e48
lacroix <jerome.lacroix@laplateforme.io>
2022-02-28 20:55:31 +0100

    depot

--- views/article.php
+++ views/article.php
@@ -5,6 +5,6 @@ $articleController = new ArticleController();
 if(isset($_GET['id_categorie'])){
     $id_categorie = $_GET["id_categorie"];
     $articleByCategorie = $articleController->getProductsByCategory($id_categorie);
-    var_dump($articleByCategorie)
+    var_dump($articleByCategorie);
 }
 
